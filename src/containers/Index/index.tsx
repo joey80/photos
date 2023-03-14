10c9bb04809b179d4d3a3bc061fd7db904250a13
jleger <j.leger@jobtarget.com>
2023-03-13 22:21:13 -0400

    Added login and index page containers

---
+++ src/containers/Index/index.tsx
@@ -0,0 +1,9 @@
+function IndexContainer() {
+  return (
+    <div>
+      <p className="text-3xl">Photos app coming soon</p>
+    </div>
+  );
+}
+
+export { IndexContainer };
---
+++ src/containers/Login/index.tsx
@@ -0,0 +1,9 @@
+function LoginContainer() {
+  return (
+    <div>
+      <p className="text-3xl">This is the login page</p>
+    </div>
+  );
+}
+
+export { LoginContainer };
